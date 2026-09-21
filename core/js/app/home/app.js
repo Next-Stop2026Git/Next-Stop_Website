e5c727dc2fedc541731162a5eca6746c7816a69d
Kenner Téllez <interlinkagency00@gmail.com>
2026-09-21 11:26:56 -0600

    Corrección de todo

--- core/js/app/home/app.js
+++ core/js/app/home/app.js
@@ -1,18 +1,11 @@
-import { initHeroStats } from './hero_config.js';
-export function initHeroReviews() {
-  const reviewContainer = document.querySelector('.hero-reviews');
-  if (!reviewContainer) return;
-}
+import { initHero } from './hero_config.js'; 
 import { initAboutAnimations } from './about-config.js';
 import { initOfficesMaps } from './offices.js';
 import { initFaqAccordion } from './faq.js';
 
-
 // En entornos de módulos de ES6, el código ya espera a que el DOM 
 // esté listo. Ejecutamos las inicializaciones directamente.
-
-initHeroStats();
-initHeroReviews();
+initHero(); 
 initAboutAnimations();
 initOfficesMaps();
 initFaqAccordion();
