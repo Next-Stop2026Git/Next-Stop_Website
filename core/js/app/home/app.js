e93cc235d1191e00ffad7b950e93a6e07d958fe0
Next-Stop-Nic <nextstopnic2026@gmail.com>
2026-09-20 18:44:26 -0600

    Add FAQ accordion initialization to app.js

--- core/js/app/home/app.js
+++ core/js/app/home/app.js
@@ -1,6 +1,8 @@
 import { initHeroStats } from './hero_config.js';
 import { initAboutAnimations } from './about-config.js';
 import { initOfficesMaps } from './offices.js';
+import { initFaqAccordion } from './faq.js';
+
 
 // En entornos de módulos de ES6, el código ya espera a que el DOM 
 // esté listo. Ejecutamos las inicializaciones directamente.
@@ -8,3 +10,4 @@ import { initOfficesMaps } from './offices.js';
 initHeroStats();
 initAboutAnimations();
 initOfficesMaps();
+initFaqAccordion();
